1fe883dde17daa0cdfd95fa7bd89dae5e41a2a9a
og <oguogu@test.com>
2020-06-09 09:57:17 +0900

    [update] ItestGenクラスにテストファイルの保存先を生成するメソッドを追加

--- TestGen/TestGenInterface.h
+++ TestGen/TestGenInterface.h
@@ -1,12 +1,24 @@
 #pragma once
 
-
+#include <fstream>
+#include <string>
 
 class ITestGen
 {
 public:
 	virtual void Generate() = 0;
-	
+
+	std::string TestFileMaker(std::string basename, int suffix)
+	{
+		std::string res;
+		res = Dir + "/" + basename + "/" + basename + "Test" + std::to_string(suffix);
+
+		return res;
+	}
+
+
+private:
+	std::string Dir = "TestResource";
 	
 	
 };
