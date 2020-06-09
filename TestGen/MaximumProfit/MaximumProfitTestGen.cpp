8550ccdda2cf2996e7a95a431bfd51e84d87bd55
og <oguogu@test.com>
2020-06-09 09:56:43 +0900

    [update] MuximumProfitTestGenのGenerateメソッドのテスト実装

--- TestGen/MaximumProfit/MaximumProfitTestGen.cpp
+++ TestGen/MaximumProfit/MaximumProfitTestGen.cpp
@@ -1,10 +1,20 @@
-
+﻿
 #include "MaximumProfitTestGen.h"
 
+#include <iostream>
 
+using namespace std;
+
+/*
+ * 制約は 2 <= N <= 200,000
+ * 1 <= R <= 10,000,000,000
+ */
 void MaximumProfitTestGen::Generate()
 {
 
+	string file = TestFileMaker("MaximumProfit", 1);
+	cout << file;
+	
 	
 }
 
