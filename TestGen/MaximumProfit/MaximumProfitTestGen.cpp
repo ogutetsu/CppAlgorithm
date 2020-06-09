d34931bc307a579bf2eb0632d9f218191973cfeb
og <oguogu@test.com>
2020-06-09 12:55:02 +0900

    [update] MaximumProfitのテスト生成を2個追加

--- TestGen/MaximumProfit/MaximumProfitTestGen.cpp
+++ TestGen/MaximumProfit/MaximumProfitTestGen.cpp
@@ -12,15 +12,49 @@ using namespace std;
 void MaximumProfitTestGen::Generate()
 {
 
+	Test1();
+	Test2();
+	Test3();
+
+	
+}
+
+void MaximumProfitTestGen::Test1()
+{
 	string file = TestFileMaker("MaximumProfit", 1);
-	cout << file;
 
 	ofstream of(file);
 
 	of << 2 << endl;
 	of << 10 << endl << 20 << endl;
-	
 
+}
+
+void MaximumProfitTestGen::Test2()
+{
+	string file = TestFileMaker("MaximumProfit", 2);
+
+	ofstream of(file);
+
+	of << 6 << endl;
+	of << 5 << endl;
+	of << 3 << endl;
+	of << 1 << endl;
+	of << 3 << endl;
+	of << 4 << endl;
+	of << 3 << endl;
+}
+
+void MaximumProfitTestGen::Test3()
+{
+	string file = TestFileMaker("MaximumProfit", 3);
+
+	ofstream of(file);
+
+	of << 3 << endl;
+	of << 4 << endl;
+	of << 3 << endl;
+	of << 2 << endl;
 	
 }
 
--- TestGen/MaximumProfit/MaximumProfitTestGen.h
+++ TestGen/MaximumProfit/MaximumProfitTestGen.h
@@ -7,6 +7,13 @@ class MaximumProfitTestGen : public ITestGen
 {
 public:
 	void Generate() override;
+
+private:
+	void Test1();
+	void Test2();
+	void Test3();
+	
+	
 };
 
 
